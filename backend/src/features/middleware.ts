aa6aacf59b8d1b476373bc71b700a926466ca633
tanahiro2010 <herentongkegu087@gmail.com>
2026-04-06 18:06:39 +0900

    feat: CORS設定をミドルウェアに追加

--- backend/src/features/middleware.ts
+++ backend/src/features/middleware.ts
@@ -14,6 +14,13 @@ const getTokenFromHeader = (c: ContextWithEnv): string | null => {
 }
 
 const middleware = async (c: ContextWithEnv, next: Next) => {
+    // Corsの設定
+    c.header('Access-Control-Allow-Origin', c.env.FRONTEND_URL);
+    c.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    c.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    c.header('Access-Control-Allow-Credentials', 'true');
+
+    // 認証トークンの検証
     const token = getTokenFromHeader(c);
     const isAuthed = token ? await verifyToken(token, c.env.JWR_SECRET ?? "DEFAULT_JWT_TOKEN") : null;
     c.set('isAuthed', isAuthed as SessionPayload | null);
